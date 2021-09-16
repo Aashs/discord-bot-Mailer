10565e1b3171c8cffe2504900f1f97a2512aade1
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-16 16:15:42 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import tasks, commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+bot = commands.Bot(command_prefix='m!', help_command=None,intents = discord.Intents.all())
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')    
+
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+    
+bot.run(getenv('TOKEN'))
---
+++ cogs/start.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class Start(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+      if ctx.guild is None and not ctx.author.bot:
+        print(ctx.content)  
+        print(ctx.author) 
+
+        embed = discord.Embed(title="Confirm Mail creation",description="This system is used for reporting bugs,reports concerning to the moderators.",color=0x3DFD1E)
+  
+        msg=await ctx.author.send(embed=embed)
+
+def setup(bot):
+  bot.add_cog(Start(bot)) 
+    
+          
