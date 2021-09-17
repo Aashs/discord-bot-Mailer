3a0552ca46dbed83bf33f57f5e6861d4afabfc0c
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-17 17:58:04 +0800

    Update bot_join.py

--- cogs/bot_join.py
+++ cogs/bot_join.py
@@ -6,6 +6,11 @@ class Start(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_guild_join(ctx)
+      
+    
 
         
         
