d620163e5bdc20083d81b082a7f79212f052479e
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-17 18:01:11 +0800

    Update bot_join.py

--- cogs/bot_join.py
+++ cogs/bot_join.py
@@ -1,18 +1,18 @@
-
 import discord
 from discord.ext import commands
 
-class Start(commands.Cog):
+class bot_join(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
     
     @commands.Cog.listener()
-    async def on_guild_join(ctx)
+    async def on_guild_join(ctx):
+        
       
     
 
         
         
 def setup(bot):
-    bot.add_cog(Start(bot)) 
+    bot.add_cog(bot_join(bot)) 
