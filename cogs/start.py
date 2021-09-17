0f9aed986c76ccd8e4da6a98a709de3dea2909c9
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-17 21:27:15 +0800

    Update start.py

--- cogs/start.py
+++ cogs/start.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
 
+class start(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+      print('Bot is ready')
+      
+      
+    @commands.command()
+    async def latency(self, ctx):
+      await ctx.send(bot.latency)
+
+
+def setup(bot):
+  bot.add_cog(start(bot)) 
