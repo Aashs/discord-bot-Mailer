901604618d76ff4daf9a5e820d4e52cce647c3be
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-18 20:30:38 +0800

    Update bot_join.py

--- cogs/bot_join.py
+++ cogs/bot_join.py
@@ -14,7 +14,7 @@ class bot_join(commands.Cog):
     
     @commands.command()
     async def latency(self, ctx):
-      await ctx.send(bot.latency)
+        await ctx.send(bot.latency)
        
         
 def setup(bot):
