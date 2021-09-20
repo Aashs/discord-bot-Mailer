07d4f27dd094d37fc35628803850b22fa887c366
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-20 13:52:31 +0800

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -8,7 +8,9 @@ class bot_join(commands.Cog):
     
     @commands.command()
     async def setup(self, ctx):
-        embed = discord.Embed(title="Setup",description=f"Created a private channel called {channel.mention'mailer-logs'} in Mailer category, use ``-accessrole[roles]`` give staff access to the channel.",color=0x1793FC)
+        await ctx.guild.create_category('Mailer')
+        await guild.create_text_channel('Mailer-logs')
+        embed = discord.Embed(title="Setup",description="Created a private channel called in Mailer category, use ``-accessrole[roles]`` give staff access to the channel.",color=0x1793FC)
         await ctx.send(embed=embed)
     
     
