22121ea450cd63d2c1669df6af603a1c277e0ed9
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-20 18:16:56 +0800

    Update setup.py

--- cogs/setup.py
+++ cogs/setup.py
@@ -12,16 +12,17 @@ class bot_join(commands.Cog):
         for guild in self.bot.guilds:
             for channel in guild.text_channels:
                 all_channels.append(channel.id)
-                
-         for y.id in all_channels:
-            if True:
-                print('ye ma boi correct')
+             
                    
         
-            x = await ctx.guild.create_category('Mailer')
-            y = await ctx.guild.create_text_channel('Mailer-logs',category=x)
-            embed = discord.Embed(title="Setup",description=f"Created a private channel {y.mention}, use ``-accessrole[roles]`` give staff access to the channel.",color=0x1793FC)
-            await ctx.send(embed=embed)
+        x = await ctx.guild.create_category('Mailer')
+        y = await ctx.guild.create_text_channel('Mailer-logs',category=x)
+        embed = discord.Embed(title="Setup",description=f"Created a private channel {y.mention}, use ``-accessrole[roles]`` give staff access to the channel.",color=0x1793FC)
+        await ctx.send(embed=embed)
+        
+        for y.id in all_channels:
+            if True:
+                print('ye ma boi correct')
         
 
     
