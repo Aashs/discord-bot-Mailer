80b9a23d9ed9407018378382a7b3f20ae3e0823c
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-21 01:47:44 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import tasks, commands
+import os
+
+bot = commands.Bot(command_prefix='-',intents = discord.Intents.all())
+
+TOKEN = os.getenv('TOKEN')
+
+@bot.command()
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send('Done')
+
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')   
+    await ctx.send('Done')
+   
+@bot.event
+async def on_ready():
+    print('Bot is ready!')
+
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(TOKEN)
