5c99faff12df14538c7bc049cbacf5af51fa960a
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-21 00:54:55 +0800

    d

--- cogs/setup.py
+++ cogs/setup.py
@@ -7,25 +7,30 @@ class bot_join(commands.Cog):
         self.bot = bot
     
     @commands.command()
-    async def setup(self, ctx):
+    async def adminlogs2(self, ctx):
         all_channels = []
+        all_categories = []
+        for category in ctx.guild.categories:
+            all_categories.append(category)
         for channel in ctx.guild.text_channels:
                 all_channels.append(channel.id)
              
-        
+        category_exists = False
+        channel_exists = False
         for i in all_channels:
-            channel = self.bot.get_channel(i)
-            category_exists = False
-            channel_exists = False
-            if channel.name == 'Mailer-logs':
+            channel = self.client.get_channel(i)
+            if channel.name == 'mailer-logs':
                 channel_exists = True
-            if category_exists == 'Mailer':
+        for i in all_categories:
+            if i.name == 'mailer':
                 category_exists = True
 
-        if category_exists and channel_exists:
+        if category_exists == True and channel_exists == True:
             print("Category and channel exists")
-        elif category_exists and channel_exists == False:
+        elif category_exists == True and channel_exists == False:
             print("Category exists but channel does not")
+        elif category_exists == False and channel_exists == True:
+            print("Category doesn't exist but channel does exist")
         else:
             print("Nothing exists. We're doomed.")
     
