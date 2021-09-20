4926012a48d6e7cf24b527015b6ff9bf6bf15019
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-21 00:25:55 +0800

    u

--- cogs/setup.py
+++ cogs/setup.py
@@ -13,19 +13,14 @@ class bot_join(commands.Cog):
             for channel in guild.text_channels:
                 all_channels.append(channel.id)
              
-                   
-        try:      
-            x = await ctx.guild.create_category('Mailer')
-            y = await ctx.guild.create_text_channel('Mailer-logs',category=x)
-            embed = discord.Embed(title="Setup",description=f"Created a private channel {y.mention}, use ``-accessrole[roles]`` give staff access to the channel.",color=0x1793FC)
-            await ctx.send(embed=embed)
-        
-        except:
-            for y.id in all_channels:
-                await ctx.send('Bot has already been setup')
-                break
-        
-
+        for y.id in all_channels:
+            print('correct')
+            break           
+ 
+        x = await ctx.guild.create_category('Mailer')
+        y = await ctx.guild.create_text_channel('Mailer-logs',category=x)
+        embed = discord.Embed(title="Setup",description=f"Created a private channel {y.mention}, use ``-accessrole[roles]`` give staff access to the channel.",color=0x1793FC)
+        await ctx.send(embed=embed)
     
     
         
