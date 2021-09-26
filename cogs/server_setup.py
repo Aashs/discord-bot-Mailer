9ad9a9d3d190636cce944c30d49706fb4c99805d
Aashes <87805252+Aashs@users.noreply.github.com>
2021-09-26 19:34:34 +0800

    Updated server_setup.py

--- cogs/server_setup.py
+++ cogs/server_setup.py
@@ -32,13 +32,22 @@ class server_setup(commands.Cog):
         if category_exists == True and channel_exists == True:
             embed = discord.Embed(description=f"Bot is already setted up",color=0xFF0000)
             await ctx.send(embed=embed)
-        else:
+        elif category_exists == True and channel_exists == False:
+            await ctx.guild.category_exists.delete()
             x = await ctx.guild.create_category('Mailer')
-            y = await ctx.guild.create_text_channel('mailer-logs',category=x)
+            await ctx.guild.create_text_channel('mailer-logs',category=x)
+            embed = discord.Embed(title="Setup",description=f"Bot has been setted up!, use ``-accessrole[roles]``to give staff access to the channel.Check out more information and configurations with ``-help``.",color=0x1793FC)
+            await ctx.send(embed=embed) 
+        elif category_exists == False and channel_exists == True:
+            print("Category doesn't exist but channel does exist")
+        else:
+            await ctx.guild.create_category('Mailer')
+            await ctx.guild.create_text_channel('mailer-logs',category=x)
 
             embed = discord.Embed(title="Setup",description=f"Bot has been setted up!, use ``-accessrole[roles]``to give staff access to the channel.Check out more information and configurations with ``-help``.",color=0x1793FC)
-            await ctx.send(embed=embed)
-    
+            await ctx.send(embed=embed) 
+
+
 
     @commands.command()
     async def access(self,ctx,arg): 
