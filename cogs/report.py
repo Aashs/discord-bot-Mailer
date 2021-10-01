f847bcc9fbeaa260b0643af899ded9a09834cea2
Aashes <87805252+Aashs@users.noreply.github.com>
2021-10-01 18:10:44 +0530

    Update report.py

--- cogs/report.py
+++ cogs/report.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from server_setup import self.bot.logs_channel
+from cogs.server_setup import self.bot.logs_channel
 
 class report(commands.Cog):
 
